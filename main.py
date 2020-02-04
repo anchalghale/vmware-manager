047349ac18c735873d7f21ef983155e3e2d513b1
Pradish <pradishbijukchhe@gmail.com>
2020-02-04 12:32:19 +0545

    Adds basic variable input

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
-
+'''Main script'''
 import os
 import subprocess
 import time
-import sys
 
 from tkinter import Tk
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, askdirectory
 from tkinter.simpledialog import askinteger
 
-
 import pygubu
+import vmrun
 
-
-# Tk().withdraw()
-# MOTHER_VM = askopenfilename()
-# if not MOTHER_VM:
-#     sys.exit()
-
-# START = askinteger('Enter an integer', 'Starting worker')
-# if not START:
-#     sys.exit()
-# END = askinteger('Enter an integer', 'Ending worker')
-# if not END:
-#     sys.exit()
 
 class Application:
-    def __init__(self, root):
+    '''The gui class of the application'''
 
-        # 1: Create a builder
+    def __init__(self, root):
         self.builder = builder = pygubu.Builder()
         root.title('VMware Manager')
         builder.add_from_file('mainframe.ui')
         builder.get_object('main_frame', root)
+        builder.connect_callbacks(self)
+
+        self.mother_vm = askopenfilename(title='Select mother VM', filetypes=['Vmx *.vmx'])
+        self.output_dir = askdirectory(title='Select output directory for VMs')
+        self.start = askinteger('Enter an integer', 'Starting worker')
+        self.end = askinteger('Enter an integer', 'Ending worker')
+
+    def clean_vms(self):
+        '''Cleans all the vms in a folder'''
+        print('hi')
+
+
+def main():
+    '''Main function of the script'''
+    root = Tk()
+    Application(root)
+    root.mainloop()
 
 
 if __name__ == '__main__':
-    root = Tk()
-    app = Application(root)
-    root.mainloop()
+    main()
